5aeb0cde26fd93e4ef50770b42edeba0c0331c03
DarkCompet <darkcompet@gmail.com>
2024-06-05 07:09:11 +0700

    Make DI better

--- src/AutoDI/RegisterDependenciesService.cs
+++ src/AutoDI/RegisterDependenciesService.cs
@@ -13,30 +13,25 @@ public static class RegisterDependenciesService {
 		return RegisterServices(filterClasses, serviceCollection);
 	}
 
-	private static string RegisterServices(
-			IEnumerable<ClassesToRegister> services,
-			IServiceCollection serviceCollection) {
+	private static string RegisterServices(IEnumerable<ClassesToRegister> services, IServiceCollection serviceCollection) {
 		Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateBootstrapLogger();
 
 		var classesRegistered = new StringBuilder();
 
-		var classesToRegisters = services.ToList();
-		foreach (var service in classesToRegisters) {
+		foreach (var service in services) {
 			if (service.ClassName != null && service.InterfaceName.Any() && !service.IgnoreInterface) {
 				AddServiceWithInterface(service, serviceCollection, classesRegistered);
 			}
-			else if ((service.ClassName != null && !service.InterfaceName.Any())
-							 || service is { ClassName: { }, IgnoreInterface: true }) {
+			else if ((service.ClassName != null && !service.InterfaceName.Any()) || service is { ClassName: { }, IgnoreInterface: true }) {
 				AddServiceWithoutInterface(service, serviceCollection, classesRegistered);
 			}
 		}
 
-		Log.Logger.Information("{OverallCount} services were registered. " +
-													 "{SingletonCount} singleton, {ScopedCount} scoped, {TransientCount} transient.",
-				classesToRegisters.Count(),
-				classesToRegisters.Count(x => x.ServiceLifetime == ServiceLifetime.Singleton),
-				classesToRegisters.Count(x => x.ServiceLifetime == ServiceLifetime.Scoped),
-				classesToRegisters.Count(x => x.ServiceLifetime == ServiceLifetime.Transient)
+		Log.Logger.Information("{OverallCount} services were registered. {SingletonCount} singleton, {ScopedCount} scoped, {TransientCount} transient.",
+			services.Count(),
+			services.Count(x => x.ServiceLifetime == ServiceLifetime.Singleton),
+			services.Count(x => x.ServiceLifetime == ServiceLifetime.Scoped),
+			services.Count(x => x.ServiceLifetime == ServiceLifetime.Transient)
 		);
 
 		return classesRegistered.ToString();
@@ -47,30 +42,31 @@ public static class RegisterDependenciesService {
 			IServiceCollection serviceCollection,
 			StringBuilder classesRegistered) {
 		foreach (var implementation in service.InterfaceName) {
-			serviceCollection.Add(
-					new ServiceDescriptor(
-							implementation,
-							service.ClassName,
-							service.ServiceLifetime));
+			serviceCollection.Add(new ServiceDescriptor(
+				implementation,
+				service.ClassName!,
+				service.ServiceLifetime)
+			);
 
 			var message = $"{service.ClassName?.Name}, {implementation} has been registered as {service.ServiceLifetime}. ";
-			Log.Logger.Information("{Message}", message);
 			classesRegistered.AppendLine(message);
+			Log.Logger.Information("{Message}", message);
 		}
 	}
 
 	private static void AddServiceWithoutInterface(
-			ClassesToRegister service,
-			IServiceCollection serviceCollection,
-			StringBuilder classesRegistered) {
-		serviceCollection.Add(
-				new ServiceDescriptor(
-						service.ClassName,
-						service.ClassName,
-						service.ServiceLifetime));
+		ClassesToRegister service,
+		IServiceCollection serviceCollection,
+		StringBuilder classesRegistered
+	) {
+		serviceCollection.Add(new ServiceDescriptor(
+			service.ClassName!,
+			service.ClassName!,
+			service.ServiceLifetime)
+		);
 
 		var message = $"{service.ClassName?.Name} has been registered as {service.ServiceLifetime}. ";
-		Log.Logger.Information("{Message}", message);
 		classesRegistered.AppendLine(message);
+		Log.Logger.Information("{Message}", message);
 	}
 }
